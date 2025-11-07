c23b7c46d8ff930ceaea3d4e1d40b886bd4dbd6a
Leo <l30leyva@gmail.com>
2025-11-06 23:48:44 -0600

    him

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -3,3 +3,23 @@
 #include "lib/kernel/bitmap.h"
 #include "threads/vaddr.h"
 #include <debug.h>
+
+static struct block *swap_block;
+static struct bitmap *swap_bitmap;
+static struct lock swap_lock;
+
+void swap_init(void) {
+  swap_block = block_get_role(BLOCK_SWAP);
+  if (swap_block == NULL) {
+    PANIC("No swap block device found");
+  }
+
+  size_t swap_size = block_size(swap_block) / (PGSIZE / BLOCK_SECTOR_SIZE);
+  swap_bitmap = bitmap_create(swap_size);
+
+  if (swap_bitmap == NULL) {
+    PANIC("Couldn't create swap bitmap");
+  }
+  
+  lock_init(&swap_lock);
+}
