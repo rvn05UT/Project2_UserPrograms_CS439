176cea2fb6865cee7284c57ce3f13fd19b2c4af4
Leo <l30leyva@gmail.com>
2025-11-09 20:00:28 -0600

    finally fixed writable bs

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -101,6 +101,7 @@ void frame_unpin(void *kpage)
 
 void *frame_alloc(void *upage, bool zero)
 {
+  ASSERT (upage != NULL);
   enum palloc_flags flags = PAL_USER | (zero ? PAL_ZERO : 0);
   void *kpage = palloc_get_page(flags);
 
